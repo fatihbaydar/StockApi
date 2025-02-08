5814dbe77f249f88c9f4b29cb8ff19f8f6ba52a5
fatihbaydar <fatihbaydar2004@gmail.com>
2025-02-08 21:35:29 +0300

    firm

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const router = require("express").Router()
 
 router.use("/user", require("./user"))
 router.use("/token", require("./token"))
+router.use("/firm", require("./firm"))
 
 module.exports = router
